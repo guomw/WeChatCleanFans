89d00f130aff62a3315d2be04e5520ad7db970fc
caijiang <luffy.ja@gmail.com>
2017-02-23 17:51:21 +0800

    新增SendTextMessageToNickName 方法以及相关测试

---
+++ WwChatHttpCore/Exceptions/LoginRequiredException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WwChatHttpCore.Exceptions
+{
+    /// <summary>
+    /// 需要登录的异常
+    /// </summary>
+    class LoginRequiredException : System.Exception
+    {
+
+    }
+}
